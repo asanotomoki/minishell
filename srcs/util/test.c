b313fbee93874edb2bcd00ca7eb2c2a2c6fe71c3
tasano <tasano@asanosatoshitakanoMacBook-Air.local>
2022-12-13 16:18:31 +0900

    add test

--- srcs/util/test.c
+++ srcs/util/test.c
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tasano <tasano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 17:10:17 by asanotomoki       #+#    #+#             */
-/*   Updated: 2022/12/13 16:15:33 by tasano           ###   ########.fr       */
+/*   Created: 2022/12/13 16:10:26 by tasano            #+#    #+#             */
+/*   Updated: 2022/12/13 16:10:56 by tasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "util.h"
 #include <stdio.h>
 
-int	perr_msg(char *msg)
+int main()
 {
-	perror(msg);
-	return (1);
-}
-
-int	err_msg(char *msg)
-{
-	ft_putendl_fd(msg, 2);
-	return (1);
+	return (0);	
 }
