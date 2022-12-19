fc420f52a90ea4089a87e6e35a48f8609a1551dd
tasano <mirai20190820@gmail.com>
2022-12-19 16:17:17 +0900

    split pipe

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,14 +6,33 @@
 /*   By: tasano <tasano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 20:38:15 by tasano            #+#    #+#             */
-/*   Updated: 2022/12/19 15:40:38 by tasano           ###   ########.fr       */
+/*   Updated: 2022/12/19 16:16:39 by tasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 #include "libft.h"
 
-char *lexer(char *line)
+t_token_lst *lexer(char *line)
 {
-	return (line);
+	t_token_lst	*lst;
+	size_t	end;
+	unsigned int start;
+
+	lst = NULL;
+	end = 0;
+	start = 0;
+	while (line[end])
+	{
+		if (line[end] == '|')
+		{
+			token_lstadd_back(&lst, token_lstnew(ft_substr(line, start, end - start), WORD));
+			token_lstadd_back(&lst, token_lstnew(ft_strdup("|"), PIPE));
+			start = end + 1;
+		}
+		end++;
+	}
+	token_lstadd_back(&lst, token_lstnew(ft_substr(line, start, end), WORD));
+
+	return (lst);
 }
