e0293f429a2616869eee3a2ee5af62c0daf731c6
Hiroaki Makino <8makino.42tokyo@gmail.com>
2023-01-14 04:45:02 +0900

    heredoc update

--- srcs/exection/set_redirect.c
+++ srcs/exection/set_redirect.c
@@ -6,18 +6,15 @@
 /*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:42:52 by tasano            #+#    #+#             */
-/*   Updated: 2023/01/14 02:33:51 by hiroaki          ###   ########.fr       */
+/*   Updated: 2023/01/14 04:42:56 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser.h"
 #include "exec.h"
 #include "../expansion/expansion.h"
 #include <fcntl.h>
-#include <string.h>
 #include <readline/readline.h>
 #include <sys/stat.h>
-#include "../../libft/includes/libft.h"
 
 void	write_heredoc(int fd, t_list *heredoc_list, int *heredoc_errno)
 {
@@ -81,6 +78,7 @@ int	use_system_pipe(t_list *heredoc_list, int *heredoc_errno)
 int	use_tempfile(t_list *heredoc_list, int *heredoc_errno)
 {
 	int			fd;
+	int			fd2;
 	struct stat	info;
 
 	errno = 0;
@@ -92,7 +90,16 @@ int	use_tempfile(t_list *heredoc_list, int *heredoc_errno)
 	}
 	fd = open(HEREDOC_FILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	write_heredoc(fd, heredoc_list, heredoc_errno);
-	return (fd);
+	fd2 = open (HEREDOC_FILE, O_RDONLY, 0600);
+	close(fd);
+	if (unlink(HEREDOC_FILE) < 0)
+	{
+		*heredoc_errno = errno;
+		close (fd2);
+		errno = *heredoc_errno;
+		return (-1);
+	}
+	return (fd2);
 }
 
 int	heredoc_to_fd(char *delimiter)
@@ -105,8 +112,6 @@ int	heredoc_to_fd(char *delimiter)
 	len = 0;
 	heredoc_errno = 0;
 	heredoc_list = creat_heredoc_list(&len, delimiter);
-	if (heredoc_list == NULL)
-		return (-1);
 	if (len == 0)
 		fd = open("/dev/null", O_RDONLY);
 	else if (len > HEREDOC_PIPESIZE)
@@ -123,30 +128,36 @@ static void	set_inredirect(t_redirect *redirect)
 	int	new_fd;
 
 	if (redirect->type == INREDIRECT)
+	{
 		new_fd = open(redirect->filename, O_RDONLY | O_CLOEXEC);
+		if (new_fd == -1)
+			perror_exit(EXIT_FAILURE, redirect->filename);
+	}
 	else
+	{
 		new_fd = heredoc_to_fd(redirect->filename);
-	if (new_fd < 0)
-		perror_exit(EXIT_FAILURE, redirect->filename);
+		if (new_fd == -1)
+			perror_exit(EXIT_FAILURE, "here document");
+	}
 	set_dup2(new_fd, STDIN_FILENO);
-	close(new_fd);
 }
 
 static void	set_outredirect(t_redirect *redirect)
 {
-	int	new_fd;
+	int		new_fd;
+	char	*f_name;
 
+	f_name = redirect->filename;
 	if (redirect->type == OUTREDIRECT)
-		new_fd = open(redirect->filename, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
+		new_fd = open(f_name, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
 	else
-		new_fd = open(redirect->filename, O_WRONLY | O_CREAT | O_CLOEXEC | O_APPEND, 0644);
+		new_fd = open(f_name, O_WRONLY | O_CREAT | O_CLOEXEC | O_APPEND, 0644);
 	if (new_fd == -1)
 		perror_exit(EXIT_FAILURE, redirect->filename);
 	set_dup2(new_fd, STDOUT_FILENO);
-	close(new_fd);
 }
 
-void set_redirect(t_redirect *redirect)
+void	set_redirect(t_redirect *redirect)
 {
 	while (redirect)
 	{
