7a4a8627bd80dad0fac6ff0265db71cf5b4c4e62
Hiroaki Makino <8makino.42tokyo@gmail.com>
2023-01-13 21:29:15 +0900

    update

--- srcs/exection/exec.h
+++ srcs/exection/exec.h
@@ -7,24 +7,23 @@
 # include <stdbool.h>
 # include <unistd.h>
 # include <sys/types.h>
-# include <parser.h>
+# include "parser.h"
 # define SHELL "minish"
 # define COMMAND_NOT_FOUND 127
 
-int	exection(t_cmd *cmd, char **envp);
+# define HEREDOC_PIPESIZE = 4096
+# define HEREDOC_FILE = ".heredoc_tempfile"
 
-
-char *get_cmdfile(char *cmd, char *path);
-
-t_cmd *convert_cmd(t_cmd *lst);
-
-void set_outredirect(t_redirect *redirect);
-void set_inredirect(t_redirect *redirect);
-void close_pipe(int pp[2]);
-void set_dup2(int new_fd, int old_fd);
-void set_pipe(int pp[2]);
-void set_waitpid(pid_t pid);
-
-void perror_exit(int status, char *msg);
-void error_exit(int status, char *msg);
+int		exection(t_cmd *cmd, char **envp);
+char	*get_cmdfile(char *cmd, char *path);
+t_cmd	*convert_cmd(t_cmd *lst);
+void	set_outredirect(t_redirect *redirect);
+void	set_inredirect(t_redirect *redirect);
+void	write_heredoc(int fd, t_list *heredoc_list, int *heredoc_errno);
+void	close_pipe(int pp[2]);
+void	set_dup2(int new_fd, int old_fd);
+void	set_pipe(int pp[2]);
+void	set_waitpid(pid_t pid);
+void	perror_exit(int status, char *msg);
+void	error_exit(int status, char *msg);
 #endif
