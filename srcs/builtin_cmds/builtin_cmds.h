7f6f434e88ffde6d1d13a2202a2f6058ad1db849
tasano <mirai20190820@gmail.com>
2023-01-15 17:09:46 +0900

    delete leaks

--- srcs/builtin_cmds/builtin_cmds.h
+++ srcs/builtin_cmds/builtin_cmds.h
@@ -6,11 +6,7 @@
 /*   By: tasano <tasano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 23:54:22 by tasano            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/01/16 01:43:10 by tasano           ###   ########.fr       */
-=======
-/*   Updated: 2023/01/15 12:46:51 by tasano           ###   ########.fr       */
->>>>>>> d6cf5c8 (builtin unset)
+/*   Updated: 2023/01/16 01:54:53 by tasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +15,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
-#include "leakdetect.h"
-#define malloc(s) leak_detelc_malloc(s, __FILE__, __LINE__)
-#define free leak_detect_free
 
-<<<<<<< HEAD
 int		builtin_echo(char **argv);
 int		builtin_exit(size_t argc, char **argv);
 int		builtin_env();
@@ -31,7 +23,8 @@ int		builtin_export(char **argv);
 int		builtin_pwd();
 int		builtin_unset(char **argv);
 int		builtin_cd(char **argv);
-=======
+
+
 int builtin_echo(char **argv);
 int builtin_exit(size_t argc, char **argv);
 int builtin_env();
@@ -40,6 +33,5 @@ int builtin_pwd();
 int builtin_unset(char **argv);
 int builtin_cd(char **argv);
 size_t search_param(char **argv, char *param);
->>>>>>> d6cf5c8 (builtin unset)
 
 #endif
