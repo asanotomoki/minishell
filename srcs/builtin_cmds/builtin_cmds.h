3db884f07cb6fb4bdc0e74998e39efe957ffe743
tasano <mirai20190820@gmail.com>
2023-01-15 17:09:46 +0900

    delete leaks

--- srcs/builtin_cmds/builtin_cmds.h
+++ srcs/builtin_cmds/builtin_cmds.h
@@ -6,7 +6,7 @@
 /*   By: tasano <tasano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 23:54:22 by tasano            #+#    #+#             */
-/*   Updated: 2023/01/15 12:46:51 by tasano           ###   ########.fr       */
+/*   Updated: 2023/01/15 17:05:54 by tasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 #include <stdio.h>
 #include <stddef.h>
-#include "leakdetect.h"
-#define malloc(s) leak_detelc_malloc(s, __FILE__, __LINE__)
-#define free leak_detect_free
 
-int builtin_echo(char **argv);
-int builtin_exit(size_t argc, char **argv);
-int builtin_env();
-int builtin_export(char **argv);
-int builtin_pwd();
-int builtin_unset(char **argv);
-int builtin_cd(char **argv);
-size_t search_param(char **argv, char *param);
+int		builtin_echo(char **argv);
+int		builtin_exit(size_t argc, char **argv);
+int		builtin_env();
+int		builtin_export(char **argv);
+int		builtin_pwd();
+int		builtin_unset(char **argv);
+int		builtin_cd(char **argv);
+
+
+size_t	search_param(char **argv, char *param);
 
 #endif
