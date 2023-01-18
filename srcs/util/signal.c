fa6e57bbc6c4718755c66196af34a18989b369ec
Hiroaki Makino <8makino.42tokyo@gmail.com>
2023-01-18 13:56:19 +0900

    add ignore SIGCHLD

--- srcs/util/signal.c
+++ srcs/util/signal.c
@@ -6,7 +6,7 @@
 /*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 00:16:16 by hiroaki           #+#    #+#             */
-/*   Updated: 2023/01/18 01:45:54 by hiroaki          ###   ########.fr       */
+/*   Updated: 2023/01/18 13:43:30 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "minishell.h"
 #include <readline/readline.h>
 #include <signal.h>
+#include <errno.h>
 
 int	rl_event(void)
 {
@@ -23,10 +24,21 @@ int	rl_event(void)
 	if (g_shell.sig_no != SIGINT)
 		return (0);
 	g_shell.sig_no = 0;
-	g_shell.status = 1;
+	if (g_shell.status == 0)
+		g_shell.status = 100;
 	rl_done = 1;
 	rl_redisplay();
 	return (0);
+}
+void ignore_signal(int signum)
+{
+	struct sigaction	sa_ignore;
+
+	sa_ignore.sa_handler = SIG_IGN;
+	sa_ignore.sa_flags = 0;
+	sigemptyset(&sa_ignore.sa_mask);
+	if (sigaction(signum, &sa_ignore, NULL) != 0)
+		exit(1);
 }
 
 void	signal_handler(int signo)
@@ -44,10 +56,24 @@ void	init_sigaction(void)
 	sigaction(SIGINT, &sa, NULL);
 }
 
+//void	init_sigaction2(void)
+//{
+//	struct sigaction	sa2;
+//
+//	sigemptyset(&sa2.sa_mask);
+//	sa2.sa_flags = 0;
+//	sa2.sa_handler = signal_handler;
+//	sigaction(SIGCHLD, &sa2, NULL);
+//}
+
 void	trap_signal(void)
 {
 	g_shell.sig_no = 0;
 	g_shell.heredoc_sig_flag = 0;
+	rl_outstream = stderr;
+	//if (isatty(STDIN_FILENO))
 	rl_event_hook = rl_event;
+	ignore_signal(SIGQUIT);
+	ignore_signal(SIGCHLD);
 	init_sigaction();
 }
